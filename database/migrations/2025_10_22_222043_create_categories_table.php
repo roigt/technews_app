095588e981b4e1cb384412fc862935ba601d2394
Godwin Julio ROIGT <griezmanjulio@yahoo.com>
2025-10-25 17:39:18 +0200

    change column slug length in category migration

--- database/migrations/2025_10_22_222043_create_categories_table.php
+++ database/migrations/2025_10_22_222043_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name',255)->unique();
-            $table->string('slug',1000)->unique();
+            $table->string('slug',255)->unique();
             $table->text('description')->nullable();
             $table->boolean('isActive')->default(true);
 
